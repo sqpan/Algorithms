a82e56e7891b0ad9e32129b0299c1b362e30f9c7
t213nk <shaoqing.pan89@gmail.com>
2018-08-15 10:50:01 -0700

    backtrack

--- src/src/subset2.java
+++ src/src/subset2.java
@@ -1,3 +1,18 @@
 public List<List<Integer>> subsetsWithDup(int[] nums) {
+	List<List<Integer>> result = new ArrayList<List<Integer>>();
+	if (nums == null || nums.length == 0) {return result;}
+	Arrays.sort(nums);
+	backtrack(result, new ArrayList<Integer>(), nums, 0);
+	return result;
+}
 
+private void backtrack(List<List<Integer>> result, List<Integer> level, int[] nums, int start) {
+	result.add(new ArrayList<Integer>(level));
+	for (int i = start; i < nums.length; i++) {
+		// if the new number is same as the previous one, and the previous is no used, continue
+		if (i > start && nums[i] == nums[i - 1]) {continue;}
+		level.add(nums[i]);
+		backtrack(result, level, nums, i + 1);
+		level.remove(level.size() - 1);
+	}
 }
